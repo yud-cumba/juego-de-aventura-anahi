dc8748d32cccfa2ca7def9c3296ccd5794e7e39a
Yuditd Cumba <60792172+yud-cumba@users.noreply.github.com>
2021-03-29 14:05:55 -0500

    le puse el primer nivel de mi juego

--- main.ts
+++ main.ts
@@ -1,23 +1,3 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    princesa.setImage(img`
-        . . . . . . f f f f 4 4 f . . . 
-        . . . . f f b f 5 4 5 5 4 f . . 
-        . . . f b 3 3 e 4 5 5 5 5 f . . 
-        . . f b 3 3 3 3 e 4 4 4 e f . . 
-        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-        . . f 3 3 3 3 e b 3 e e 3 3 f . 
-        . . f 3 3 3 3 f f e e e 3 3 f . 
-        . . f b b b b f b f e e e 3 f . 
-        . . f b b b b e 1 f 4 4 e f . . 
-        . f f b b b b f 4 4 4 4 f . . . 
-        . f b b b b f f f e e e f . . . 
-        . . f b b f 4 4 e d d d f . . . 
-        . . . f f e 4 4 e d d d f . . . 
-        . . . . f b e e b d b d b f . . 
-        . . . . f f d 1 d 1 d 1 f f . . 
-        . . . . . . f f b b f f . . . . 
-        `)
-})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     princesa.setImage(img`
         . . . . . . . . . . . . . . . . 
@@ -36,6 +16,26 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
         . . f d d d b b e e b b f . . . 
         . . f b d 1 d 1 d d b f . . . . 
         . . . f f f b b f f f . . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    princesa.setImage(img`
+        . . . . . . f f f f 4 4 f . . . 
+        . . . . f f b f 5 4 5 5 4 f . . 
+        . . . f b 3 3 e 4 5 5 5 5 f . . 
+        . . f b 3 3 3 3 e 4 4 4 e f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . . f b b b b f b f e e e 3 f . 
+        . . f b b b b e 1 f 4 4 e f . . 
+        . f f b b b b f 4 4 4 4 f . . . 
+        . f b b b b f f f e e e f . . . 
+        . . f b b f 4 4 e d d d f . . . 
+        . . . f f e 4 4 e d d d f . . . 
+        . . . . f b e e b d b d b f . . 
+        . . . . f f d 1 d 1 d 1 f f . . 
+        . . . . . . f f b b f f . . . . 
         `)
 })
 let princesa: Sprite = null
@@ -180,3 +180,4 @@ princesa = sprites.create(img`
     . . . . . . f f b b f f . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(princesa, 100, 0)
+tiles.setTilemap(tilemap`level1`)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,10 +2,39 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`2000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004000000000202020202020202020202020202020202020202020202020202020200000002000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000401010101010303030101010303030101010103030301010103030101010101010101010101030303010101030303010101010303030101010303010101010101`, img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 
+. . . . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . 2 . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 
+. . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 
+2 2 2 2 2 . . . 2 2 2 . . . 2 2 2 2 . . . 2 2 2 . . 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,sprites.dungeon.chestClosed], TileScale.Sixteen);
+        }
+        return null;
+    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
         }
         return null;
     })
